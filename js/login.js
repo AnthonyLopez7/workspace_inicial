047a4d789e86436f9d495bc694af376162f444f4
AnthonyLopez <alopedrozo07@gmail.com>
2020-08-15 23:07:20 -0300

    Pantalla login y validación concluida

--- js/login.js
+++ js/login.js
@@ -1,6 +1,21 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
-});
+// onclick="location.href='index.html'"
+// onclick="validation"
+
+// document.addEventListener("DOMContentLoaded", function(e){
+// });
+
+function validar() {
+    var usuario = document.getElementById("usuario").value;
+    var Contraseña = document.getElementById("pass").value;
+
+    if (usuario == "" || Contraseña == "") {
+        alert("Debe completar todos los campos");
+    }
+    else {
+        location.href='index.html'
+    }
+}
