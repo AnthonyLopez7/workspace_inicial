88d1d0be5374c7a347659b9171b0251866fe5dbc
AnthonyLopez <alopedrozo07@gmail.com>
2020-08-15 21:06:05 -0300

    Pantalla de Productos concluida

--- js/products.js
+++ js/products.js
@@ -1,6 +1,47 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var categoriesArray = [];
 
+function showProductsList(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let products = array[i];
+
+        htmlContentToAppend += `
+        <div class="hola">
+            <hr>
+            <div class="productContainer">
+                <div class="itemsContainer">
+                    <div class="imageContainer">
+                        <img src="` + products.imgSrc + `" alt="Image of vehicle loading" class="image">
+                    </div>
+                    <div class="informationContainer">
+                        <div class="nameDescription">
+                            <h4 class="mb-1">`+ products.name + `</h4>
+                            <p>`+ products.description + `</p>
+                        </div>
+                        <div class="coinPrice">
+                            <small class="text-muted">` + products.soldCount + ` artículos</small>
+                            <p>`+ products.currency + ` ` + products.cost + `</p>
+                        <div/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("product_container").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            categoriesArray = resultObj.data;
+            //Muestro los productos ordenadas
+            showProductsList(categoriesArray);
+        }
+    });
 });
